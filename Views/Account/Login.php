849630909f78468245243364fec8849546045896
Junior possari <junior.possari@pro4tech.com.br>
2021-05-26 16:09:00 -0300

    Tentando imitar o mvc

---
+++ Views/Account/Login.php
@@ -0,0 +1,1 @@
+<center>Olá Mundo!</center>
---
+++ Views/Home/Index.php
@@ -0,0 +1,7 @@
+
+
+<div class="form-group">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome" class="form-control" placeholder="Nome" >
+    <small id="helpId" class="text-muted">Nome de usuário</small>
+</div>
