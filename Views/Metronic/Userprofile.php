f96105280530ef4513379ccfdcad76c942c8e1f3
Junior possari <junior.possari@pro4tech.com.br>
2021-05-30 13:32:06 -0300

    Att userprofile

--- Views/Metronic/Userprofile.php
+++ Views/Metronic/Userprofile.php
@@ -12,7 +12,7 @@
 	<div class="offcanvas-content pr-5 mr-n5">
 		<!--begin::Header-->
 		<div class="d-flex align-items-center mt-5">
-			<div class="symbol symbol-100 mr-5 border border-primary">
+			<div class="symbol symbol-100 mr-5 border border-dark" style="border-width:2px !important;">
 				<img class="symbol-label" src="<?php if(isset($Foto)){ echo 'data:image/jpeg;base64,'.base64_encode($Foto); }else{ echo '/Noteflix/Content/img/sem-foto.png'; } ?>">				
 				<img class="symbol-badge bg-success"></>
 			</div>
@@ -54,11 +54,10 @@
 						<div class="symbol-label">
 							<span class="svg-icon svg-icon-md svg-icon-success">
 								<!--begin::Svg Icon | path:Metronic/demo2/dist/assets/media/svg/icons/General/Notification2.svg-->
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+								<span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo2/dist/../src/media/svg/icons/General/Settings-2.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
 									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-										<rect x="0" y="0" width="24" height="24" />
-										<path d="M13.2070325,4 C13.0721672,4.47683179 13,4.97998812 13,5.5 C13,8.53756612 15.4624339,11 18.5,11 C19.0200119,11 19.5231682,10.9278328 20,10.7929675 L20,17 C20,18.6568542 18.6568542,20 17,20 L7,20 C5.34314575,20 4,18.6568542 4,17 L4,7 C4,5.34314575 5.34314575,4 7,4 L13.2070325,4 Z" fill="#000000" />
-										<circle fill="#000000" opacity="0.3" cx="18.5" cy="5.5" r="2.5" />
+										<rect x="0" y="0" width="24" height="24"/>
+										<path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#000000"/>
 									</g>
 								</svg>
 								<!--end::Svg Icon-->
@@ -66,9 +65,8 @@
 						</div>
 					</div>
 					<div class="navi-text">
-						<div class="font-weight-bold">My Profile</div>
-						<div class="text-muted">Account settings and more
-						<span class="label label-light-danger label-inline font-weight-bold">update</span></div>
+						<div class="font-weight-bold">Conta</div>
+						<div class="text-muted">Configurações da conta</div>
 					</div>
 				</div>
 			</a>
@@ -80,13 +78,10 @@
 						<div class="symbol-label">
 							<span class="svg-icon svg-icon-md svg-icon-warning">
 								<!--begin::Svg Icon | path:Metronic/demo2/dist/assets/media/svg/icons/Shopping/Chart-bar1.svg-->
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+								<span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo2/dist/../src/media/svg/icons/General/Star.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
 									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-										<rect x="0" y="0" width="24" height="24" />
-										<rect fill="#000000" opacity="0.3" x="12" y="4" width="3" height="13" rx="1.5" />
-										<rect fill="#000000" opacity="0.3" x="7" y="9" width="3" height="8" rx="1.5" />
-										<path d="M5,19 L20,19 C20.5522847,19 21,19.4477153 21,20 C21,20.5522847 20.5522847,21 20,21 L4,21 C3.44771525,21 3,20.5522847 3,20 L3,4 C3,3.44771525 3.44771525,3 4,3 C4.55228475,3 5,3.44771525 5,4 L5,19 Z" fill="#000000" fill-rule="nonzero" />
-										<rect fill="#000000" opacity="0.3" x="17" y="11" width="3" height="6" rx="1.5" />
+										<polygon points="0 0 24 0 24 24 0 24"/>
+										<path d="M12,18 L7.91561963,20.1472858 C7.42677504,20.4042866 6.82214789,20.2163401 6.56514708,19.7274955 C6.46280801,19.5328351 6.42749334,19.309867 6.46467018,19.0931094 L7.24471742,14.545085 L3.94038429,11.3241562 C3.54490071,10.938655 3.5368084,10.3055417 3.92230962,9.91005817 C4.07581822,9.75257453 4.27696063,9.65008735 4.49459766,9.61846284 L9.06107374,8.95491503 L11.1032639,4.81698575 C11.3476862,4.32173209 11.9473121,4.11839309 12.4425657,4.36281539 C12.6397783,4.46014562 12.7994058,4.61977315 12.8967361,4.81698575 L14.9389263,8.95491503 L19.5054023,9.61846284 C20.0519472,9.69788046 20.4306287,10.2053233 20.351211,10.7518682 C20.3195865,10.9695052 20.2170993,11.1706476 20.0596157,11.3241562 L16.7552826,14.545085 L17.5353298,19.0931094 C17.6286908,19.6374458 17.263103,20.1544017 16.7187666,20.2477627 C16.5020089,20.2849396 16.2790408,20.2496249 16.0843804,20.1472858 L12,18 Z" fill="#000000"/>
 									</g>
 								</svg>
 								<!--end::Svg Icon-->
@@ -94,8 +89,8 @@
 						</div>
 					</div>
 					<div class="navi-text">
-						<div class="font-weight-bold">My Messages</div>
-						<div class="text-muted">Inbox and tasks</div>
+						<div class="font-weight-bold">Favoritos</div>
+						<div class="text-muted">Séries e filmes favoritos</div>
 					</div>
 				</div>
 			</a>
@@ -119,37 +114,12 @@
 						</div>
 					</div>
 					<div class="navi-text">
-						<div class="font-weight-bold">My Activities</div>
-						<div class="text-muted">Logs and notifications</div>
+						<div class="font-weight-bold">Atividades</div>
+						<div class="text-muted">Atividades recentes</div>
 					</div>
 				</div>
 			</a>
-			<!--end:Item-->
-			<!--begin::Item-->
-			<a href="Metronic/demo2/dist/custom/apps/userprofile-1/overview.html" class="navi-item">
-				<div class="navi-link">
-					<div class="symbol symbol-40 bg-light mr-3">
-						<div class="symbol-label">
-							<span class="svg-icon svg-icon-md svg-icon-primary">
-								<!--begin::Svg Icon | path:Metronic/demo2/dist/assets/media/svg/icons/Communication/Mail-opened.svg-->
-								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-										<rect x="0" y="0" width="24" height="24" />
-										<path d="M6,2 L18,2 C18.5522847,2 19,2.44771525 19,3 L19,12 C19,12.5522847 18.5522847,13 18,13 L6,13 C5.44771525,13 5,12.5522847 5,12 L5,3 C5,2.44771525 5.44771525,2 6,2 Z M7.5,5 C7.22385763,5 7,5.22385763 7,5.5 C7,5.77614237 7.22385763,6 7.5,6 L13.5,6 C13.7761424,6 14,5.77614237 14,5.5 C14,5.22385763 13.7761424,5 13.5,5 L7.5,5 Z M7.5,7 C7.22385763,7 7,7.22385763 7,7.5 C7,7.77614237 7.22385763,8 7.5,8 L10.5,8 C10.7761424,8 11,7.77614237 11,7.5 C11,7.22385763 10.7761424,7 10.5,7 L7.5,7 Z" fill="#000000" opacity="0.3" />
-										<path d="M3.79274528,6.57253826 L12,12.5 L20.2072547,6.57253826 C20.4311176,6.4108595 20.7436609,6.46126971 20.9053396,6.68513259 C20.9668779,6.77033951 21,6.87277228 21,6.97787787 L21,17 C21,18.1045695 20.1045695,19 19,19 L5,19 C3.8954305,19 3,18.1045695 3,17 L3,6.97787787 C3,6.70173549 3.22385763,6.47787787 3.5,6.47787787 C3.60510559,6.47787787 3.70753836,6.51099993 3.79274528,6.57253826 Z" fill="#000000" />
-									</g>
-								</svg>
-								<!--end::Svg Icon-->
-							</span>
-						</div>
-					</div>
-					<div class="navi-text">
-						<div class="font-weight-bold">My Tasks</div>
-						<div class="text-muted">latest tasks and projects</div>
-					</div>
-				</div>
-			</a>
-			<!--end:Item-->
+			<!--end:Item-->				
 		</div>
 		<!--end::Nav-->
 		<!--begin::Separator-->
@@ -158,7 +128,7 @@
 		<!--begin::Notifications-->
 		<div>
 			<!--begin:Heading-->
-			<h5 class="mb-5">Recent Notifications</h5>
+			<h5 class="mb-5">Notificações</h5>
 			<!--end:Heading-->
 			<!--begin::Item-->
 			<div class="d-flex align-items-center bg-light-warning rounded p-5 gutter-b">
