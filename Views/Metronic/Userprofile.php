452bb54890617cc85247df679bb28e17b2afcea4
Junior possari <junior.possari@pro4tech.com.br>
2021-05-31 22:13:48 -0300

    Rm conta do perfil

--- Views/Metronic/Userprofile.php
+++ Views/Metronic/Userprofile.php
@@ -59,31 +59,7 @@
 		<div class="separator separator-dashed mt-8 mb-5"></div>
 		<!--end::Separator-->
 		<!--begin::Nav-->
-		<div class="navi navi-spacer-x-0 p-0">
-			<!--begin::Item-->
-			<a href="Metronic/demo2/dist/custom/apps/user/profile-1/personal-information.html" class="navi-item">
-				<div class="navi-link">
-					<div class="symbol symbol-40 bg-light mr-3">
-						<div class="symbol-label">
-							<span class="svg-icon svg-icon-md svg-icon-success">
-								<!--begin::Svg Icon | path:Metronic/demo2/dist/assets/media/svg/icons/General/Notification2.svg-->
-								<span class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo2/dist/../src/media/svg/icons/General/Settings-2.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-									<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-										<rect x="0" y="0" width="24" height="24"/>
-										<path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#000000"/>
-									</g>
-								</svg>
-								<!--end::Svg Icon-->
-							</span>
-						</div>
-					</div>
-					<div class="navi-text">
-						<div class="font-weight-bold">Conta</div>
-						<div class="text-muted">Configurações da conta</div>
-					</div>
-				</div>
-			</a>
-			<!--end:Item-->
+		<div class="navi navi-spacer-x-0 p-0">			
 			<!--begin::Item-->
 			<a href="Metronic/demo2/dist/custom/apps/user/profile-3.html" class="navi-item">
 				<div class="navi-link">
