bf446621d4c3dbebc0bd7f8f0bd846c3dc4720f1
Junior possari <junior.possari@pro4tech.com.br>
2021-06-07 12:58:14 -0300

    Nova bd

--- Services/ConexaoService.php
+++ Services/ConexaoService.php
@@ -10,7 +10,7 @@
 
             if(!isset(self::$instancia)){
 
-                $dbname = 'noteflix';
+                $dbname = 'noteflixbd';
                 $host = 'localhost';
                 $user = 'root';
                 $password = '';
